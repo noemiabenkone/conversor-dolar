b73dc55a95e96a98eac026e623af96ec59e30a17
Noemia <noemiabenkone0@gmail.com>
2025-02-10 15:57:06 -0300

    lidando com js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { Container, Title, Converter, Input, Button} from "./styles.js";
 
 function App() {
+  const [valueReal, setValueReal] = useState(0)
+
+  const handleConverte = () => {
+    console.log(valueReal/5.79)
+  }
+  const handleValueReal = (evento) =>{
+    setValueReal(evento.target.value)
+  }
   return (
     <Container>
       <Title>Conversor Dólar</Title>
       <Converter>
-        <Input type="number" placeholder="Valor em real" />
-        <Button>Converter</Button>
+        <Input value={valueReal} onChange={handleValueReal} type="number" placeholder="Valor em real" />
+        <Button onClick={handleConverte}>Converter</Button>
       </Converter>
+      {valueReal}
     </Container>
   );
 }
